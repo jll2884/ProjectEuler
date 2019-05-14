c324b60bda2ee7d4d6d00d86118133a3fe9b734b
jll2884 <simplylopez7@gmail.com>
2019-05-13 22:05:56 -0400

    new updates

--- EvenFibonnaciNumbers.py
+++ EvenFibonnaciNumbers.py
@@ -5,7 +5,6 @@ def fibonacci():
         newNum = lst[-1]+lst[-2]
         if(newNum%2==0):
             sum+=newNum
-            print(newNum)
         lst.append(newNum)
     print("the sum of the even numbers is: "+str(sum))
     print(lst)
